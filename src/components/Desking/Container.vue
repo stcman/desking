d066800ce9516acecf9cf996ef6af2e1079ce1ee
tad@briq.com <tad@briq.com>
2024-04-04 00:00:51 -0400

    added desking component

---
+++ src/components/Desking/Container.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="DeskContainer">
+      <div class="desk-sections">Test</div>
+      <div class="desk-sections">Test</div>
+      <div class="desk-sections">Test</div>
+      <div class="desk-sections">Test</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DeskingContainer',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  #DeskContainer {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  </style>
+  
